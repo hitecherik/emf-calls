7dab9c9c4a452f97cc25ac25b2f229d9f5e0448d
Alex Nielsen <hi@alexnielsen.me>
2024-05-31 18:10:13 +0100

    Add merlot

--- internal/led/led.go
+++ internal/led/led.go
@@ -3,20 +3,22 @@ package led
 type LedStatus *[3]byte
 
 var (
-	StatusOff   LedStatus = &[3]byte{0, 0, 0}
-	StatusRed   LedStatus = &[3]byte{255, 0, 0}
-	StatusGreen LedStatus = &[3]byte{0, 255, 0}
-	StatusBlue  LedStatus = &[3]byte{0, 0, 255}
-	StatusWhite LedStatus = &[3]byte{255, 255, 255}
+	StatusOff    LedStatus = &[3]byte{0, 0, 0}
+	StatusRed    LedStatus = &[3]byte{255, 0, 0}
+	StatusGreen  LedStatus = &[3]byte{0, 255, 0}
+	StatusBlue   LedStatus = &[3]byte{0, 0, 255}
+	StatusWhite  LedStatus = &[3]byte{255, 255, 255}
+	StatusMerlot LedStatus = &[3]byte{110, 51, 69}
 )
 
 var (
 	StatusTranslation = map[string]LedStatus{
-		"off":   StatusOff,
-		"red":   StatusRed,
-		"green": StatusGreen,
-		"blue":  StatusBlue,
-		"white": StatusWhite,
+		"off":    StatusOff,
+		"red":    StatusRed,
+		"green":  StatusGreen,
+		"blue":   StatusBlue,
+		"white":  StatusWhite,
+		"merlot": StatusMerlot,
 	}
 
 	currentLedStatus = StatusWhite
