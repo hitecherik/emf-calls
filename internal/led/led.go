89a328e5ee1292b4b3499b19c3eaba53686f708c
Alex Nielsen <hi@alexnielsen.me>
2024-06-01 01:21:15 +0100

    Add colour inspired by 404 bytes

--- internal/led/led.go
+++ internal/led/led.go
@@ -3,24 +3,26 @@ package led
 type LedStatus *[3]byte
 
 var (
-	StatusOff    LedStatus = &[3]byte{0, 0, 0}
-	StatusRed    LedStatus = &[3]byte{255, 0, 0}
-	StatusGreen  LedStatus = &[3]byte{0, 255, 0}
-	StatusBlue   LedStatus = &[3]byte{0, 0, 255}
-	StatusWhite  LedStatus = &[3]byte{255, 255, 255}
-	StatusMerlot LedStatus = &[3]byte{110, 51, 20}
-	StatusOrange LedStatus = &[3]byte{255, 165, 0}
+	StatusOff      LedStatus = &[3]byte{0, 0, 0}
+	StatusRed      LedStatus = &[3]byte{255, 0, 0}
+	StatusGreen    LedStatus = &[3]byte{0, 255, 0}
+	StatusBlue     LedStatus = &[3]byte{0, 0, 255}
+	StatusWhite    LedStatus = &[3]byte{255, 255, 255}
+	StatusMerlot   LedStatus = &[3]byte{110, 51, 20}
+	StatusOrange   LedStatus = &[3]byte{255, 165, 0}
+	StatusNotFound LedStatus = &[3]byte{60, 33, 68}
 )
 
 var (
 	StatusTranslation = map[string]LedStatus{
-		"off":    StatusOff,
-		"red":    StatusRed,
-		"green":  StatusGreen,
-		"blue":   StatusBlue,
-		"white":  StatusWhite,
-		"merlot": StatusMerlot,
-		"orange": StatusOrange,
+		"off":       StatusOff,
+		"red":       StatusRed,
+		"green":     StatusGreen,
+		"blue":      StatusBlue,
+		"white":     StatusWhite,
+		"merlot":    StatusMerlot,
+		"orange":    StatusOrange,
+		"not found": StatusNotFound,
 	}
 
 	currentLedStatus = StatusOrange
